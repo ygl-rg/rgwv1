8add2306c2ecb45fcec05a7ca86c107667f735f8
ygl <ygl@desktop.com>
2019-03-02 19:53:39 +0800

    clean up

--- __main__.py
+++ __main__.py
@@ -1,14 +1,4 @@
-import sys
-
-
 if __name__ == "__main__":
-    args = sys.argv
-    if args[1] == 'rgw_node':
-        import rgw_go
-        rgw_go.main()
-    elif args[1] == 'rgw_init':
-        import init_rgw_go
-        init_rgw_go.main()
-    else:
-        print("process type missing")
+    import run_rgw
+    run_rgw.main()
 
